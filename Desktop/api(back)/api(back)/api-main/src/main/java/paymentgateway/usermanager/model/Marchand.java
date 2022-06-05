d05d1abd2f950830aa60df7ba1721b93ba3a9047
Youssef <youssef.belahouel123@gmail.com>
2022-06-05 15:05:29 +0100

    first commit

---
+++ Desktop/api(back)/api(back)/api-main/src/main/java/paymentgateway/usermanager/model/Marchand.java
@@ -0,0 +1,30 @@
+package paymentgateway.usermanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@ToString
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Marchand implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false,updatable = false)
+    private String marchandCode;
+
+
+
+}
